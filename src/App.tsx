9dfb6492829bf33bc31d2ba5387060093868db13
hrkd <hiroki@hrkd.net>
2022-05-19 23:26:32 +0900

    :construction: wip

--- src/App.tsx
+++ src/App.tsx
@@ -2,39 +2,42 @@ import React, {useEffect, useRef, useMemo, useState} from 'react';
 import logo from './logo.svg';
 import './App.css';
 import * as THREE from 'three';
-import Ascii from './Ascii';
+import { AsciiEffect } from 'three/examples/jsm/effects/AsciiEffect';
 
 function App() {
   const ref = useRef<HTMLCanvasElement>(null);
   let renderer: THREE.WebGLRenderer;
+  let effect: AsciiEffect;
   const camera = new THREE.PerspectiveCamera(45, window.innerWidth / window.innerHeight, 1, 2000);
   const scene = new THREE.Scene();
   let geo: THREE.PlaneBufferGeometry;
   let red = new THREE.MeshBasicMaterial({ color: new THREE.Color(0xff0000) });
   let plane: THREE.Mesh;
   const loader = new THREE.ImageBitmapLoader();
-  loader.setOptions( { imageOrientation: 'flipY' } );
+  loader.setOptions({ imageOrientation: 'flipY' });
   let canvas;
-  let context:WebGL2RenderingContext | null | undefined;
+  let context: WebGL2RenderingContext | null | undefined;
 
   let mat: THREE.MeshBasicMaterial;
 
   const resizeHandler = () => {
-    const w = window.innerWidth
+    const w = window.innerWidth;
     const h = window.innerHeight;
-    geo = new THREE.PlaneBufferGeometry(window.innerWidth , window.innerHeight);
+    geo = new THREE.PlaneBufferGeometry(window.innerWidth, window.innerHeight);
     renderer.setPixelRatio(window.devicePixelRatio);
-    renderer.setSize(w/20, h/20);
+    renderer.setSize(w, h);
+    effect.setSize(w, h);
+
     camera.aspect = w / h;
     camera.updateProjectionMatrix();
-  }
+  };
 
-  const scrollHandler = (ev:Event) => {
+  const scrollHandler = (ev: Event) => {
     camera.position.setY(-window.scrollY);
-  }
+  };
 
   useEffect(() => {
-    geo = new THREE.PlaneBufferGeometry(window.innerWidth , window.innerHeight);
+    geo = new THREE.PlaneBufferGeometry(window.innerWidth, window.innerHeight);
 
     window.addEventListener('scroll', scrollHandler);
 
@@ -43,9 +46,9 @@ function App() {
       './pexels-michael-morse-7536592.jpg',
 
       // onLoad callback
-      function ( imageBitmap ) {
-        const texture = new THREE.CanvasTexture( imageBitmap );
-        mat = new THREE.MeshBasicMaterial( { map: texture } );
+      function (imageBitmap) {
+        const texture = new THREE.CanvasTexture(imageBitmap);
+        mat = new THREE.MeshBasicMaterial({ map: texture });
         plane = new THREE.Mesh(geo, mat);
         scene.add(plane);
       },
@@ -54,14 +57,14 @@ function App() {
       undefined,
 
       // onError callback
-      function ( err ) {
-        console.log( 'An error happened' );
+      function (err) {
+        console.log('An error happened');
       }
     );
     return () => {
       window.removeEventListener('resize', resizeHandler);
       window.removeEventListener('scroll', scrollHandler);
-    }
+    };
   }, []);
 
   useEffect(() => {
@@ -69,42 +72,50 @@ function App() {
     const canvas = ref.current;
 
     renderer = new THREE.WebGLRenderer({ canvas, preserveDrawingBuffer: true });
-    
+    effect = new AsciiEffect(renderer, ' .:-+*=%@#', { invert: false });
+
     resizeHandler();
     renderer.setClearColor(new THREE.Color(0x000000), 1);
+    const wrapper = document.querySelector('#root');
+    wrapper?.appendChild(effect.domElement);
+    effect.domElement.setAttribute('class', 'ascii');
+
     window.addEventListener('resize', resizeHandler);
 
     const animate = () => {
-      requestAnimationFrame( animate );
-      camera.position.z = window.innerHeight/2/Math.tan(camera.fov/2 * Math.PI / 180.0);
+      // setTimeout(() => {
+      requestAnimationFrame(animate);
+      // }, 1000 / 30);
+      camera.position.z = window.innerHeight / 2 / Math.tan(((camera.fov / 2) * Math.PI) / 180.0);
 
-      renderer.render(scene, camera)
+      // renderer.render(scene, camera);
+      effect.render(scene, camera);
       // console.log(window.pixels);
       // renderer.getContext().readPixels(0, 0, window.innerWidth/20, window.innerHeight/20, renderer.getContext().RGBA, renderer.getContext().UNSIGNED_BYTE, window.pixels);
+      // ascii.update();
     };
     animate();
   }, [ref]);
 
   return (
     <>
-    <canvas ref={ref}></canvas>
-      <Ascii/>
-    <div className="App" style={{ height: 10000 }}>
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+      <canvas ref={ref}></canvas>
+      <div className="App" style={{ height: 10000 }}>
+        <header className="App-header">
+          <img src={logo} className="App-logo" alt="logo" />
+          <p>
+            Edit <code>src/App.tsx</code> and save to reload.
+          </p>
+          <a
+            className="App-link"
+            href="https://reactjs.org"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            Learn React
+          </a>
+        </header>
+      </div>
     </>
   );
 }
