9776f50a01a2b5886eefa20664eef6be71d5695f
hrkd <hiroki@hrkd.net>
2022-05-19 17:40:42 +0900

    :construction: wip

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,8 @@
-import React, {useEffect, useRef} from 'react';
+import React, {useEffect, useRef, useMemo, useState} from 'react';
 import logo from './logo.svg';
 import './App.css';
-import { isJSDocReturnTag } from 'typescript';
 import * as THREE from 'three';
+import Ascii from './Ascii';
 
 function App() {
   const ref = useRef<HTMLCanvasElement>(null);
@@ -10,15 +10,21 @@ function App() {
   const camera = new THREE.PerspectiveCamera(45, window.innerWidth / window.innerHeight, 1, 2000);
   const scene = new THREE.Scene();
   let geo: THREE.PlaneBufferGeometry;
-  let red = new THREE.MeshBasicMaterial({ color: new THREE.Color(0xff0000), wireframe:true });
+  let red = new THREE.MeshBasicMaterial({ color: new THREE.Color(0xff0000) });
   let plane: THREE.Mesh;
+  const loader = new THREE.ImageBitmapLoader();
+  loader.setOptions( { imageOrientation: 'flipY' } );
+  let canvas;
+  let context:WebGL2RenderingContext | null | undefined;
+
+  let mat: THREE.MeshBasicMaterial;
 
   const resizeHandler = () => {
     const w = window.innerWidth
     const h = window.innerHeight;
     geo = new THREE.PlaneBufferGeometry(window.innerWidth , window.innerHeight);
     renderer.setPixelRatio(window.devicePixelRatio);
-    renderer.setSize(w, h);
+    renderer.setSize(w/20, h/20);
     camera.aspect = w / h;
     camera.updateProjectionMatrix();
   }
@@ -29,10 +35,29 @@ function App() {
 
   useEffect(() => {
     geo = new THREE.PlaneBufferGeometry(window.innerWidth , window.innerHeight);
-    plane = new THREE.Mesh(geo, red);
-    scene.add(plane);
 
     window.addEventListener('scroll', scrollHandler);
+
+    loader.load(
+      // resource URL
+      './pexels-michael-morse-7536592.jpg',
+
+      // onLoad callback
+      function ( imageBitmap ) {
+        const texture = new THREE.CanvasTexture( imageBitmap );
+        mat = new THREE.MeshBasicMaterial( { map: texture } );
+        plane = new THREE.Mesh(geo, mat);
+        scene.add(plane);
+      },
+
+      // onProgress callback currently not supported
+      undefined,
+
+      // onError callback
+      function ( err ) {
+        console.log( 'An error happened' );
+      }
+    );
     return () => {
       window.removeEventListener('resize', resizeHandler);
       window.removeEventListener('scroll', scrollHandler);
@@ -41,23 +66,29 @@ function App() {
 
   useEffect(() => {
     if (!ref.current) return;
+    const canvas = ref.current;
 
-    renderer = new THREE.WebGLRenderer({canvas: ref.current})
+    renderer = new THREE.WebGLRenderer({ canvas, preserveDrawingBuffer: true });
+    
     resizeHandler();
-    renderer.setClearColor(new THREE.Color(0x0000FF), 1);
+    renderer.setClearColor(new THREE.Color(0x000000), 1);
     window.addEventListener('resize', resizeHandler);
 
     const animate = () => {
-      requestAnimationFrame(animate);
+      requestAnimationFrame( animate );
       camera.position.z = window.innerHeight/2/Math.tan(camera.fov/2 * Math.PI / 180.0);
 
       renderer.render(scene, camera)
+      // console.log(window.pixels);
+      // renderer.getContext().readPixels(0, 0, window.innerWidth/20, window.innerHeight/20, renderer.getContext().RGBA, renderer.getContext().UNSIGNED_BYTE, window.pixels);
     };
     animate();
   }, [ref]);
+
   return (
     <>
-      <canvas ref={ref}></canvas>
+    <canvas ref={ref}></canvas>
+      <Ascii/>
     <div className="App" style={{ height: 10000 }}>
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
