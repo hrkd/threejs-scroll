ef2fc58b7842657b10af48c94d38302281e3670b
hrkd <hiroki@hrkd.net>
2022-05-24 19:47:05 +0900

    :sparkles: add fisheye effect

--- src/App.tsx
+++ src/App.tsx
@@ -2,15 +2,23 @@ import React, {useEffect, useRef} from 'react';
 import logo from './logo.svg';
 import './App.css';
 import * as THREE from 'three';
+import { EffectComposer } from 'three/examples/jsm/postprocessing/EffectComposer';
+import { RenderPass } from 'three/examples/jsm/postprocessing/RenderPass';
+import { ShaderPass } from 'three/examples/jsm/postprocessing/ShaderPass';
+import * as MathUtils from 'three/src/math/MathUtils';
 import GUI from 'lil-gui'; 
+import { getDistortionShaderDefinition } from './shaders/getDistortionShaderDefinition';
 
 function App() {
   const ref = useRef<HTMLCanvasElement>(null);
   let renderer: THREE.WebGLRenderer;
+  let composer: EffectComposer;
+  let effect:ShaderPass;
+
   const camera = new THREE.PerspectiveCamera(45, window.innerWidth / window.innerHeight, 1, 2000);
   const scene = new THREE.Scene();
   let geo: THREE.PlaneBufferGeometry;
-  let red = new THREE.MeshBasicMaterial({ color: new THREE.Color(0xff0000), wireframe:true });
+  let red = new THREE.MeshBasicMaterial({ color: new THREE.Color(0xff0000), wireframe:false});
   let plane: THREE.Mesh;
   const gui = new GUI();
 
@@ -19,7 +27,9 @@ function App() {
     const h = window.innerHeight;
     geo = new THREE.PlaneBufferGeometry(window.innerWidth , window.innerHeight);
     renderer.setPixelRatio(window.devicePixelRatio);
+    composer.setPixelRatio(window.devicePixelRatio);
     renderer.setSize(w, h);
+    composer.setSize(w, h);
     camera.aspect = w / h;
     camera.updateProjectionMatrix();
   }
@@ -44,6 +54,24 @@ function App() {
     if (!ref.current) return;
 
     renderer = new THREE.WebGLRenderer({canvas: ref.current})
+    composer = new EffectComposer( renderer );
+    composer.addPass( new RenderPass( scene, camera ) );
+    effect = new ShaderPass( getDistortionShaderDefinition() );
+    composer.addPass( effect );
+    effect.renderToScreen = true;
+
+    // Setup distortion effect
+    var horizontalFOV = 140;
+    var strength = 0.25;
+    var cylindricalRatio = 2;
+    var height = Math.tan(MathUtils.degToRad(horizontalFOV) / 2) / camera.aspect;
+    camera.fov = Math.atan(height) * 2 * 180 / 3.1415926535;
+
+    effect.uniforms[ "strength" ].value = strength;
+    effect.uniforms[ "height" ].value = height;
+    effect.uniforms[ "aspectRatio" ].value = camera.aspect;
+    effect.uniforms[ "cylindricalRatio" ].value = cylindricalRatio;
+
     resizeHandler();
     renderer.setClearColor(new THREE.Color(0xEEEEEE), 1);
     window.addEventListener('resize', resizeHandler);
@@ -53,6 +81,7 @@ function App() {
       camera.position.z = window.innerHeight/2/Math.tan(camera.fov/2 * Math.PI / 180.0);
 
       renderer.render(scene, camera)
+      composer.render();
     };
     animate();
   }, [ref]);
