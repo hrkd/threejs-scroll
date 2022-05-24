602b9d15e34331778ace732bb2d7700f57f2f8cc
hrkd <hiroki@hrkd.net>
2022-05-24 20:13:39 +0900

    :sparkles: use gui

--- src/App.tsx
+++ src/App.tsx
@@ -18,9 +18,12 @@ function App() {
   const camera = new THREE.PerspectiveCamera(45, window.innerWidth / window.innerHeight, 1, 2000);
   const scene = new THREE.Scene();
   let geo: THREE.PlaneBufferGeometry;
-  let red = new THREE.MeshBasicMaterial({ color: new THREE.Color(0xff0000), wireframe:false});
+  let red = new THREE.MeshBasicMaterial({ color: new THREE.Color(0xff0000), wireframe:true});
   let plane: THREE.Mesh;
   const gui = new GUI();
+  gui.add({
+    strength: 0.25
+  }, 'strength',0,1 );
 
   const resizeHandler = () => {
     const w = window.innerWidth
@@ -39,7 +42,7 @@ function App() {
   }
 
   useEffect(() => {
-    geo = new THREE.PlaneBufferGeometry(window.innerWidth , window.innerHeight);
+    geo = new THREE.PlaneBufferGeometry(window.innerWidth , window.innerHeight,10,10);
     plane = new THREE.Mesh(geo, red);
     scene.add(plane);
 
@@ -62,15 +65,18 @@ function App() {
 
     // Setup distortion effect
     var horizontalFOV = 140;
-    var strength = 0.25;
     var cylindricalRatio = 2;
     var height = Math.tan(MathUtils.degToRad(horizontalFOV) / 2) / camera.aspect;
     camera.fov = Math.atan(height) * 2 * 180 / 3.1415926535;
 
-    effect.uniforms[ "strength" ].value = strength;
+    effect.uniforms[ "strength" ].value = 0.25;
     effect.uniforms[ "height" ].value = height;
     effect.uniforms[ "aspectRatio" ].value = camera.aspect;
     effect.uniforms[ "cylindricalRatio" ].value = cylindricalRatio;
+
+    gui.onChange(ev => {
+      if(effect.uniforms[ "strength" ].value)effect.uniforms[ "strength" ].value = ev.value
+    })
 
     resizeHandler();
     renderer.setClearColor(new THREE.Color(0xEEEEEE), 1);
